0362b6a3b5d1988064a6c71bbdcb7adf7ed28733
root <root@testingpackerv2.europe-west1-c.c.week10-group-team1.internal>
2018-09-27 14:31:01 +0000

    Refactoration

--- getindex.sh
+++ getindex.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd ~
 git clone https://github.com/Willsi/Ugly-Basic-Pokemon-Database-Website.git
-find /home/georgeec96/packer-lamp-server/Ugly-Basic-Pokemon-Database-Website -type f -exec cp {} /var/www/html \;
+find /packer-lamp-server/Ugly-Basic-Pokemon-Database-Website -type f -exec cp {} /var/www/html \;
 #sudo cp -r /home/vagrant/packer-lamp-server/gcp-website/* /var/www/html
