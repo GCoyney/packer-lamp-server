6d5d5e240a697385c3db8542620a7b2d226ab06b
root <root@testingpackerv2.europe-west1-c.c.week10-group-team1.internal>
2018-09-27 14:16:36 +0000

    New commit :(

--- apache.sh
+++ apache.sh
@@ -3,7 +3,13 @@ apt-get update
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password your_password'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password_again your_password'
 sudo apt-get -y install mysql-server
-apt-get install -y apache2
-apt-get -y install php7.0 libapache2-mod-php7.0
+echo "mysql server installed -------------------------------------------------------------------"
+echo "=========================================================================================="
+sudo apt-get install -y apache2
+echo "apache2 installed -------------------------------------------------------------------"
+echo "=========================================================================================="
+sudo apt-get -y install php7.0 libapache2-mod-php7.0
+echo "php installed -------------------------------------------------------------------"
+echo "=========================================================================================="
 sudo systemctl restart apache2
 sudo systemctl enable apache2
--- getindex.sh
+++ getindex.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone https://github.com/user-sachin/gcp-website.git
-sudo cp -r /home/vagrant/packer-lamp-server/gcp-website/* /var/www/html
+find /home/georgeec96/packer-lamp-server/gcp-website -type f -exec cp {} /var/www/html \;
+#sudo cp -r /home/vagrant/packer-lamp-server/gcp-website/* /var/www/html
