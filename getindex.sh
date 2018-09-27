df60b5a47ac1cee2a854db54d89012f892fbff9c
root <root@testingpackerv2.europe-west1-c.c.week10-group-team1.internal>
2018-09-27 14:20:05 +0000

    Uses different app

--- getindex.sh
+++ getindex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/user-sachin/gcp-website.git
+git clone https://github.com/Willsi/Ugly-Basic-Pokemon-Database-Website.git
 find /home/georgeec96/packer-lamp-server/gcp-website -type f -exec cp {} /var/www/html \;
 #sudo cp -r /home/vagrant/packer-lamp-server/gcp-website/* /var/www/html
